00f1e214091147eb5c720686397e716a8420469f
GPLer <2240686290@qq.com>
2018-10-21 21:26:44 +0800

    Update Main.java

--- src/pc/virtual/hardware/Main.java
+++ src/pc/virtual/hardware/Main.java
@@ -7,6 +7,7 @@ public class Main {
     vKeyboard vK = new vKeyboard();
     vMouse vM = new vMouse();
     System.out.println("Hello,Human.");
+    vM.test();
     vK.openCalcToC("999*666-2333");
   }
 }

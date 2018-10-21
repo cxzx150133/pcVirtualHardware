d26ea2f58694465f1727863476fa212b9f8c2f81
GPLer <2240686290@qq.com>
2018-10-21 21:22:46 +0800

    Update Main.java

--- src/pc/virtual/hardware/Main.java
+++ src/pc/virtual/hardware/Main.java
@@ -3,18 +3,10 @@ package pc.virtual.hardware;
 import java.awt.AWTException;
 
 public class Main {
-	public static void main(String[] args) throws AWTException {
-		vKeyboard vK = new vKeyboard();
-		//vK.keyBoardLightRolls(10, 200);
-		//vK.demo_a_girl_selling_matches();
-		//
-		//vK.openTaskmgr();
-		//vK.openRunner();
-		//vK.inputString("cd D:\\ && D:",200);
-		//vK.cmdRunner("cd D:\\ && D:");
-		//vK.powershellRunner("date");
-		//
-		System.out.println("Hello,Human.");
-		vK.openCalcToC("999*666-2333");
-	}
+  public static void main(String[] args) throws AWTException {
+    vKeyboard vK = new vKeyboard();
+    vMouse vM = new vMouse();
+    System.out.println("Hello,Human.");
+    vK.openCalcToC("999*666-2333");
+  }
 }
